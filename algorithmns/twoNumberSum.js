e4e592f2eeb9b080010c94100cfc8a548556b4b1
masete <nicholasmasete72@gmail.com>
2022-06-17 13:10:50 +0300

    added big O

--- algorithmns/twoNumberSum.js
+++ algorithmns/twoNumberSum.js
@@ -178,3 +178,36 @@ function twoNumberSum(array, targetSum) {
 
 console.log(twoNumberSum([9,1,3,4,5], 6)) // [1,5]
 console.log(twoNumberSum([1,2,3,4,5], 10)) // []
+
+/**
+ * Here we're sorting the algorithm before we iterate it.
+ *  And then we only iterate it once, using a pointer at each extreme of the array and iterating 
+ * "inwards".
+
+This is better than the solution before, since we're only iterating once.
+ But we're still sorting the array (which usually has a logarithmic complexity) 
+ and then iterating once (which is linear complexity). 
+The algorithmic complexity of this solution is O(n log(n)).
+ */
+
+
+function twoNumberSum(array, targetSum) {
+    // Iterate over array once, and at each iteration
+    // check if the number you need to get to ther target exists in the array
+    // If it exists, return its index and the present number index
+	let result = []
+
+	for (let i = 0; i < array.length; i++) {
+        let desiredNumber = targetSum - array[i]
+        if (array.indexOf(desiredNumber) !== -1 && array.indexOf(desiredNumber) !== i) {
+            result.push(array[i])
+            result.push(array[array.indexOf(desiredNumber)])
+            break
+        }
+	}
+
+    return result
+}
+
+console.log(twoNumberSum([9,1,3,4,5], 6)) // [1,5]
+console.log(twoNumberSum([1,2,3,4,5], 10)) // []
